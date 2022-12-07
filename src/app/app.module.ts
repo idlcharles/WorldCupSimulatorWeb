37fd93f5b1ac6f616db7616cfe1db498cce6c638
Charles Dos Santos <charles.idlsc@gmail.com>
2022-12-07 06:37:47 -0300

    Aula 2 Iniciando o front End da aplicacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {DragDropModule } from '@angular/cdk/drag-drop'
 
 import { AppComponent } from './app.component';
+import { GroupsComponent } from './groups/groups.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/groups/groups.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {moveItemInArray } from'@angular/cdk/drag-drop';
+import { Team } from '../Models/Team';
+
+@Component({
+  selector: 'app-groups',
+  templateUrl: './groups.component.html',
+  styleUrls: ['./groups.component.scss']
+})
+export class GroupsComponent implements OnInit{
+
+  constructor(){}
+  groups!: Team[][];
+
+  ngOnInit(): void {
+   this.initGroup()
+  }
+
+  initGroup(){
+    this.groups = [];
+    for(var i = 0; i < 8; i++){
+     this.groups[i] = [
+       {name:'', img: ''},
+       {name:'', img: ''},
+       {name:'', img: ''},
+       {name:'', img: ''}
+     ]
+    }
+  }
+  drop(event: any, index: number){
+    moveItemInArray(this.groups[index], event.previousIndex, event.currentIndex)
+  }
+}
+
+
