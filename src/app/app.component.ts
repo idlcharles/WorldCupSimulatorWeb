bd7b2408c93fe4371fc94f388a10043777fb9e57
Charles Dos Santos <charles.idlsc@gmail.com>
2022-12-19 21:28:54 -0300

    estilizando as chaves e organização dos times ate o vencedor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Match, Team } from './Models/Team';
 import { Component, OnInit } from '@angular/core';
 import { TeamsService } from './services/teams.service';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -31,8 +32,7 @@ constructor(service:TeamsService) {
     this.initWinner();
   }
 onMoveTeam(groups: any) {
- this.populateKnockouts(groups);
-  this.initRoundOf16();
+  this.populateKnockouts(groups);
   this.initQuarter();
   this.initSemi();
   this.initFinal();
@@ -40,21 +40,77 @@ onMoveTeam(groups: any) {
 }
 
   changeQuarter(lastMatch: number, winner: Team) {
+    if(this.roundOf16[lastMatch].teamOne?.name == ''||
+      this.roundOf16[lastMatch].teamTwo?.name == '')
+      return;
+
+var currentMatch = this.calculateNextMatch(lastMatch);
+var isTeamOneSide = this.isteamOneSideNextMatch(lastMatch);
+
+if(isTeamOneSide)
+this.quarter[currentMatch].teamOne = winner
+else
+this.quarter[currentMatch].teamTwo = winner
+
+var nextMatch = this.calculateNextMatch(currentMatch);
+var loser = this.roundOf16[lastMatch].teamOne!.name == winner.name ?
+this.roundOf16[lastMatch].teamTwo : this.roundOf16[lastMatch].teamOne
+
+if(this.semi[nextMatch].teamOne == loser || this.semi[nextMatch].teamTwo == loser)
+this.changeSemi(currentMatch, winner!, loser);
 
   }
 
   changeSemi(lastMatch: number, winner: Team, loser: Team | undefined = undefined) {
+    if(this.quarter[lastMatch].teamOne?.name == ''||
+    this.quarter[lastMatch].teamTwo?.name == '')
+    return;
 
+    var currentMatch = this.calculateNextMatch(lastMatch);
+    var isTeamOneSide = this.isteamOneSideNextMatch(lastMatch);
+
+    if(isTeamOneSide)
+    this.semi[currentMatch].teamOne = winner
+    else
+    this.semi[currentMatch].teamTwo = winner
+
+    if(loser == undefined)
+    loser = this.quarter[lastMatch].teamOne!.name == winner.name?
+    this.quarter[lastMatch].teamTwo : this.quarter[lastMatch].teamOne
+
+    if (this.final.teamOne == loser || this.final.teamTwo == loser)
+    this.changeFinal(currentMatch,winner, loser);
   }
 
   changeFinal(lastMatch: number, winner: Team, loser: Team | undefined = undefined) {
+    if(this.semi[lastMatch].teamOne?.name == ''||
+    this.semi[lastMatch].teamTwo?.name == '')
+    return;
+
+switch(lastMatch) {
+  case 0:
+    this.final.teamOne =winner
+    break;
+    case 1:
+      this.final.teamTwo = winner
+      break;
+}
+if(loser == undefined)
+    loser = this.semi[lastMatch].teamOne!.name == winner.name?
+    this.semi[lastMatch].teamTwo : this.semi[lastMatch].teamOne
+
+    if (this.winner == loser)
+    this.changeWinner(winner);
 
   }
 
   changeWinner(winner: Team) {
+    if(this.final.teamOne?.name == ''||
+    this.final.teamTwo?.name == '')
+    return;
 
+    this.winner = winner;
   }
-
   initRoundOf16(){
     this.roundOf16 = [];
     for(var i = 0; i < 8; i++){
@@ -110,6 +166,44 @@ this.winner = {name:'', img:''}
     this.roundOf16.push({teamOne: groups[5][0], teamTwo: groups[4][1]});
     this.roundOf16.push({teamOne: groups[6][0], teamTwo: groups[7][1]});
     this.roundOf16.push({teamOne: groups[7][0], teamTwo: groups[6][1]});
+    }
+    calculateNextMatch(match: number) {
+      switch(match)
+      {
+        case 0:
+          return 0;
+          case 2:
+          return 0;
+          case 1:
+          return 1;
+          case 3:
+          return 1;
+          case 4:
+          return 2;
+          case 6:
+          return 2;
+          case 5:
+          return 3;
+          case 7:
+          return 3;
+          default:
+            return 0;
+      }
+    }
+    isteamOneSideNextMatch(lastMatch: number){
+      switch(lastMatch){
+        case 0:
+        case 1:
+        case 4:
+        case 5:
+        return true;
+        case 2:
+        case 3:
+        case 6:
+        case 7:
+        default:
+          return false;
+      }
     }
 }
 
